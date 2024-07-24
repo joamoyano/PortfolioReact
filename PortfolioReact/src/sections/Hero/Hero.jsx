ffdae56e8e9164ec837d2bdeca1de6144331fdcf
joamoyano <125034082+joamoyano@users.noreply.github.com>
2024-07-24 19:13:58 -0300

    hero en progreso

--- PortfolioReact/src/sections/Hero/Hero.jsx
+++ PortfolioReact/src/sections/Hero/Hero.jsx
@@ -1,11 +1,28 @@
 import styles from  './HeroStyles.module.css';
+import heroImg from '../../assets/hero-img.png';
+import themeIcon from '../../assets/sun.svg';
+import githubIcon from '../../assets/github-light.svg';
+import LinkedinIcon from '../../assets/linkedin-light.svg';
+
 function Hero() {
   return (
-    <div>
-        <h1>Hero</h1>
-      {/* Hero specific content */}
-      
-    </div>
+   <section id="hero">
+    <div className={styles.colorModeContainer}>
+      <img className= {styles.hero} src={heroImg} alt="Perfile picture of Joaquin Moyano" />
+      <img className={styles.colorMode} src={themeIcon} alt="Color made icon" />
+   </div>
+   <div className={styles.info}>
+    <h1>Joaquin <br></br>Moyano</h1>
+    <h2>Developer</h2>
+    <span>
+      <a href="https://github.com/joamoyano" target="_blank" rel="noopener noreferrer"><img src={githubIcon} alt='Github icon'/></a> 
+
+      <a href="https://www.linkedin.com/in/joaquin-moyano-cba/" target="_blank" rel="noopener noreferrer"><img src={LinkedinIcon} alt='Linkedin icon'/></a> 
+     
+      <a href="mailto:joamoyano@gmail.com">Email</a>
+    </span>
+   </div>
+   </section>
   )
 }
 
