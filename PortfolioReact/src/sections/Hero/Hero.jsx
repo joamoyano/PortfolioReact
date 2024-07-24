ead071f15a9fcbeea3ad6e811d42bffff7e0cffb
joamoyano <125034082+joamoyano@users.noreply.github.com>
2024-07-24 02:41:58 -0300

    portfolio nuevo en progreso

---
+++ PortfolioReact/src/sections/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import styles from  './HeroStyles.module.css';
+function Hero() {
+  return (
+    <div>
+        <h1>Hero</h1>
+      {/* Hero specific content */}
+      
+    </div>
+  )
+}
+
+export default Hero
