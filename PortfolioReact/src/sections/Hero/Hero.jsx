816fab1ab34fd7fb5c3f6c7e6e0e36c81ef33f24
joamoyano <125034082+joamoyano@users.noreply.github.com>
2024-07-25 21:59:01 -0300

    dando estilo al hero

--- PortfolioReact/src/sections/Hero/Hero.jsx
+++ PortfolioReact/src/sections/Hero/Hero.jsx
@@ -1,15 +1,33 @@
 import styles from  './HeroStyles.module.css';
 import heroImg from '../../assets/hero-img.png';
-import themeIcon from '../../assets/sun.svg';
-import githubIcon from '../../assets/github-light.svg';
-import LinkedinIcon from '../../assets/linkedin-light.svg';
+import sun from '../../assets/sun.svg';
+import moon from '../../assets/moon.svg';
+import githubLight from '../../assets/github-light.svg';
+import githubDark from '../../assets/github-dark.svg';
+import linkedinLight from '../../assets/linkedin-light.svg';
+import linkedinDark from '../../assets/linkedin-dark.svg';
+import emailLight from '../../assets/email-light.svg';
+import emailDark from '../../assets/email-dark.svg';
+import CV from '../../assets/JoaquinMoyanoCV.pdf';
+import { useTheme } from '../../common/ThemeContext';
 
 function Hero() {
+  const {theme, toggleTheme} = useTheme();
+
+  const themeIcon = theme === 'light' ? sun : moon;
+  const emailIcon = theme === 'light'? emailLight : emailDark;
+  const githubIcon = theme === 'light'? githubLight : githubDark;
+  const LinkedinIcon = theme === 'light'? linkedinLight : linkedinDark;
+  
+  
   return (
-   <section id="hero">
+   <section id="hero" className={styles.container}>
     <div className={styles.colorModeContainer}>
       <img className= {styles.hero} src={heroImg} alt="Perfile picture of Joaquin Moyano" />
-      <img className={styles.colorMode} src={themeIcon} alt="Color made icon" />
+      <img className={styles.colorMode} src={themeIcon} alt="Color made icon" 
+        onClick= {toggleTheme}
+      
+      />
    </div>
    <div className={styles.info}>
     <h1>Joaquin <br></br>Moyano</h1>
@@ -19,9 +37,13 @@ function Hero() {
 
       <a href="https://www.linkedin.com/in/joaquin-moyano-cba/" target="_blank" rel="noopener noreferrer"><img src={LinkedinIcon} alt='Linkedin icon'/></a> 
      
-      <a href="mailto:joamoyano@gmail.com">Email</a>
+      <a href="mailto:joamoyano@gmail.com"target="_blank" rel="noopener noreferrer"><img src={emailIcon} alt='email icon'/></a>
     </span>
    </div>
+   <p>Mejorando día a día para crear proyectos que inspiran y resuelven problemas</p>
+   <a href={CV} download>
+    <button className='hover' >Descargar CV</button>  
+   </a>
    </section>
   )
 }
