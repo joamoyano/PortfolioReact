0e57397808e615fe4cbe6c51fcea6a2fceb92e11
joamoyano <125034082+joamoyano@users.noreply.github.com>
2024-07-27 23:23:00 -0300

    add Projects

--- PortfolioReact/src/App.jsx
+++ PortfolioReact/src/App.jsx
@@ -1,9 +1,15 @@
+import './App.css';
 import Hero from './sections/Hero/Hero';
-import './App.css'
+import Projects from './sections/Projects/Projects';
 
 function App() {
- 
-  return <Hero></Hero>
+  return (
+    <>
+      <Hero />
+      <Projects />
+
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ PortfolioReact/src/sections/Projects/Projects.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Styles from './ProjectsStyles.module.css'
+export default function Projects() {
+  return (
+    <section id="Projects" className={Styles.container}>
+      <h1 className={Styles}>Projects</h1>
+    </section>
+  )
+}
