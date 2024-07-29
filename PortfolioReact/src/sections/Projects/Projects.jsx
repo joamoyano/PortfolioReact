3e4e6577e1c8a88a8878682c4b58b0855b877ba4
joamoyano <125034082+joamoyano@users.noreply.github.com>
2024-07-28 23:05:56 -0300

    add Projects

--- PortfolioReact/src/sections/Projects/Projects.jsx
+++ PortfolioReact/src/sections/Projects/Projects.jsx
@@ -4,6 +4,12 @@ export default function Projects() {
   return (
     <section id="Projects" className={Styles.container}>
       <h1 className={Styles}>Projects</h1>
+
+      <div className={Styles.project}>
+        <img src="https://via.placeholder.com/200" alt="Project 1" />
+        <h2>Project 1</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
     </section>
   )
 }
